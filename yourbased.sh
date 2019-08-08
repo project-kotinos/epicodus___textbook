42c97c52cff1a32bfde98bb7d1e05a3d1ffeb7c5
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-08 17:03:00 -0400

    Create yourbased.sh

---
+++ yourbased.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -ex
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get install -y tzdata unzip
+gem install bundler -v 2.0.1
+# before_install
+gem update --system
+gem install bundler
+gem --version
+wget http://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+rm chromedriver_linux64.zip
+sudo mv -f chromedriver /usr/local/bin/
+sudo chmod +x /usr/local/bin/chromedriver
+google-chrome-stable --headless --no-sandbox
+# install
+bundle install --jobs=3 --retry=3
+# before_script
+bundle exec rake db:create db:schema:load
+# script
+rake
