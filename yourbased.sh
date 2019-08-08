254c658cc263e3e296d86bb0b525ef8b9c81bc2f
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-08 17:19:02 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 export DEBIAN_FRONTEND=noninteractive
-apt-get update && apt-get install -y tzdata unzip
+apt-get update && apt-get install -y unzip chromium-chromedriver
 gem install bundler -v 2.0.1
 # before_install
 gem update --system
